befb3e51310f482f6d4a7737e3997c34ea88d97c
DT Pham <dt99.pham@gmail.com>
2020-08-28 21:56:44 -0700

    Add evaluation function

--- Adversarial_Search/minimax.py
+++ Adversarial_Search/minimax.py
@@ -1,4 +1,23 @@
-from minimax_helpers import *
+player_id = 0
+
+def my_moves(gameState):
+    return len(gameState.actions())
 
 def minimax_decision(gameState, depth):
-  return max(gameState.actions(), key a: min_value(gameState.result(a), depth))
+    return max(gameState.actions(), key = lambda a: min_value(gameState.result(a), depth-1))
+
+def min_value(gameState, depth):
+    """ Return the game state utility if the game is over,
+    otherwise return the minimum value over all legal successors
+    """
+    if gameState.terminal_test(): return gameState.utility(0)
+    if depth == 0: return my_moves(gameState)
+    return min(max_value(gameState.result(a), depth-1) for a in gameState.actions())
+
+def max_value(gameState, depth):
+    """ Return the game state utility if the game is over,
+    otherwise return the maximum value over all legal successors
+    """
+    if gameState.terminal_test(): return gameState.utility(0)
+    if depth == 0: return my_moves(gameState)
+    return max(min_value(gameState.result(a), depth-1) for a in gameState.actions())
